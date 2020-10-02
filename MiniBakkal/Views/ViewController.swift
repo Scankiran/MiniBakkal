eab8e0dafd54464fa3b0a865e03729999809aea7
Said <cank32cank@gmail.com>
2020-10-02 22:02:36 +0300

    Project Created and Pod init

---
+++ MiniBakkal/Views/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniBakkal
+//
+//  Created by Said Çankıran on 2.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
